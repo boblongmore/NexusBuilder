c81ae93c593e35693fb10cbacb2f47ee3dab1bd8
boblongmore <robertlongmore@gmail.com>
2017-04-07 11:25:55 -0500

    added function

--- py_render.py
+++ py_render.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
 from jinja2 import Template  
-import yaml  
+import yaml
+import os
 
+def config_render():
+    #Parse the YAML file and produce a Python dict.
+    yaml_vars = yaml.load(open('nexus.yml').read())
 
-#Parse the YAML file and produce a Python dict.
-yaml_vars = yaml.load(open('nexus.yml').read())
+    #Load the Jinja2 template into a Python data structure.
+    template = Template(open('nexus9k.j2').read())
 
-#Load the Jinja2 template into a Python data structure.
-template = Template(open('nexus9k.j2').read())
+    #Render the configuration using the Jinja2 render method using yaml_vars as arg.
+    rendered_config = template.render(yaml_vars)
 
-#Render the configuration using the Jinja2 render method using yaml_vars as arg.
-rendered_config = template.render(yaml_vars)
+    #Write the rendered configuration to a text file.
+    config_name = yaml_vars['hostname']
+    with open(config_name, 'w') as config:
+        config.write(rendered_config)
+    if os.path.isfile(config_name):
+        print "The configuration file, %s, is present." % config_name
+    else:
+        print "The configuration file, %s,  is not present." % config_name
 
-#Write the rendered configuration to a text file.
-with open('9k_test_config', 'w') as config:  
-    config.write(rendered_config)
+if __name__ == "__main__":
+    config_render()
